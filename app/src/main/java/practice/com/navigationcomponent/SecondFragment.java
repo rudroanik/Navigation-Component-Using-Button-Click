de96caf5e60452389011a089b2739402250757eb
rudroanik <rudroaiub@gmail.com>
2019-03-25 10:33:27 +0600

    init commit

---
+++ app/src/main/java/practice/com/navigationcomponent/SecondFragment.java
@@ -0,0 +1,33 @@
+package practice.com.navigationcomponent;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.navigation.Navigation;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SecondFragment extends Fragment {
+
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =inflater.inflate(R.layout.fragment_second, container, false);
+        view.findViewById(R.id.floatingID).setOnClickListener(Navigation.createNavigateOnClickListener(R.id.next_action));
+        return view;
+    }
+
+}
